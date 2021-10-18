3f38cd2227a2f45fdcdb78797d317af6a99e360f
Guillem Delas <guillemdelas@hotmail.com>
2021-10-18 19:51:14 +0200

    CREATE checkouts controller and ADD the checkout fn which adds laptops to the cart and shows the total price depending on the payment method selected (still has to be improved)

---
+++ app/controllers/api/v1/checkouts_controller.rb
@@ -0,0 +1,53 @@
+class Api::V1::CheckoutsController < ApplicationController
+  def checkout
+    codes = params[:ids]
+    if codes.present?
+      laptops = Laptop.find_laptops(params[:ids])
+
+      price = basic_price(laptops)
+      
+      # calculate payment methods
+      if two_lenovos?(codes)
+        total_price = buy_one_get_one_free(price)
+      elsif two_or_more_macbooks?(codes)
+        total_price = macbook_discount(price)
+      else
+        total_price = price
+      end
+
+      render json: {
+        message: 'This is your bill. Come back soon!',
+        cart: laptops,
+        total_price: "The total amount to pay is #{total_price}€" 
+      }
+    end
+  end
+
+  private
+
+  def two_lenovos?(codes)
+    codes.count('LN1') >= 2
+  end
+
+  def two_or_more_macbooks?(codes)
+    codes.count('AP1') >= 2
+  end
+
+  def macbook_discount(price)
+    price * 0.9
+  end
+
+  def buy_one_get_one_free(price)
+    price - 41
+  end
+
+  def basic_price(laptops)
+    price = 0
+
+    laptops.each do |laptop|
+      price += laptop.price.to_i
+    end
+
+    return price
+  end
+end
--- app/models/laptop.rb
+++ app/models/laptop.rb
@@ -4,4 +4,16 @@ class Laptop < ApplicationRecord
     validates :name, presence: true, uniqueness: true
     validates :code, format: { with: /\A[A-Z]{2}[1-9]{1}\z/, message: "'%{value}' must be in uppercase and it should contain 2 letters and 1 digit at the end" }
     validates :price, presence: true, numericality: { greater_than: 0 }
+
+
+    def self.find_laptops(codes)
+        laptops = []
+        codes.each do |code|
+            laptop = Laptop.find_by(code: code)
+
+            laptops << laptop if (laptop.present?)
+        end
+
+        return laptops
+    end
 end
