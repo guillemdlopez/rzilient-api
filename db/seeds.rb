f7263b35d2f91a7c9dbfb8cbb71312dc03304b2f
Guillem Delas <guillemdelas@hotmail.com>
2021-10-18 18:59:05 +0200

    ADD some test data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Laptop.destroy_all
+
+laptop_names = ['Macbook Pro 13', 'Lenovo Thinkpad', 'HP 850 G5']
+laptop_codes = ['AP1', 'LN1', 'HP1']
+laptop_prices = [60, 41, 39]
+
+
+puts "Generating some test data..."
+
+laptop_names.each_with_index do |laptop, i|
+    Laptop.create!(code: laptop_codes[i], name: laptop, price: laptop_prices[i])
+end
+
+
+puts "We are done! :)"
+puts "#{Laptop.count} laptops were created!"
