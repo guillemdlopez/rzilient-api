2cdb63bb29a3230f1d671662c8235601a3c46f62
Guillem Delas <guillemdelas@hotmail.com>
2021-10-18 19:00:06 +0200

    ADD the route of the checkout api endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  namespace :api, defaults: { format: :json } do
+    namespace :v1 do
+      post "checkouts", to: "checkouts#checkout"
+    end
+  end
 end
